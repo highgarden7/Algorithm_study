3168bde4c1c2296a45426a6203030453fa8b67f3
highgarden7 <37359858+highgarden7@users.noreply.github.com>
2023-06-16 11:01:44 +0900

    백준 bronze 1110 (java)

---
+++ backjoon/bronze/1110/1110.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class algo {
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+
+    private static void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int a = 0;
+        int b = 0;
+
+        if (n < 10) {
+            b = n;
+        } else {
+            a = n / 10;
+            b = n % 10;
+        }
+
+        int cycle = 1;
+
+        int temp = 0;
+        while (true) {
+            temp = (a + b) % 10;
+            if ((b * 10 + temp) == n) {
+                break;
+            }
+            a = b;
+            b = temp;
+            cycle++;
+        }
+
+        System.out.println(cycle);
+    }
+}
